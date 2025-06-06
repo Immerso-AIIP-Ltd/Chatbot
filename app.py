6a63cb84f529e16fa5b8276fea5368709047d4fb
ramkumar-eros <ramkumar.vallimayil@erosinvest.com>
2025-06-06 10:25:35 +0530

    Fix heroku deployment

--- app.py
+++ app.py
@@ -195,5 +195,6 @@ def ayurveda_consult():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
